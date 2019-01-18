506fc391ceab68be94f3e88fa5bb6135306e2ca2
akhilesh <akhilespande@gmail.com>
2019-01-18 11:12:44 +0530

    sop statements added

--- Test.java
+++ Test.java
@@ -3,5 +3,12 @@ public class Test{
 public static void main(String args[]){
 
 System.out.println("Welcome to first java program");
+
+System.out.println("Welcome to Second java program");
+
+System.out.println("Welcome to Third java program");
+
+System.out.println("Welcome to Fourth java program");
+
 }
 }
